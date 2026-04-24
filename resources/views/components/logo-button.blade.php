82bc79f5f17411b531e3507ed6807e29fda422b4
Cimplex-DiazMinton <minton.diaz@cimplex.com.ph>
2026-04-25 02:47:12 +0800

    remove branding

--- resources/views/components/logo-button.blade.php
+++ resources/views/components/logo-button.blade.php
@@ -1,13 +1,10 @@
 <a href="/home" class="flex items-center gap-2">
     <div class="w-full h-full flex gap-3">
         <span class="w-8 h-8 rounded-md bg-yellow-500 flex items-center justify-center">
-            <svg class="w-5 h-5 text-gray-800" fill="currentColor" viewBox="0 0 20 20">
-                <path
-                    d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h3a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h3a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
-            </svg>
+
         </span>
         <span class="font-display font-bold text-3xl tracking-tight">
-            Land<span class="text-yellow-500">wise</span>
+            LOGO
         </span>
 
     </div>
